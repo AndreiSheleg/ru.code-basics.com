99695e93943a95426a0f9b05aa7fcd0e4fe9fbe1
Andrei <106683554+AndreiSheleg@users.noreply.github.com>
2022-06-06 23:21:58 +0300

    feat: update ex049.js file

--- ex049.js
+++ ex049.js
@@ -1,1 +1,7 @@
-
+// BEGIN (write your solution here)
+// Проверка функции truncate
+const truncate = (variableLine, len) => {
+  const result = `${variableLine.slice(0, len)}...`;
+  return result;
+};
+// END
