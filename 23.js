298f3099fa4d995c074bf568c50388fdc48103a5
Andrei <106683554+AndreiSheleg@users.noreply.github.com>
2022-10-01 11:45:01 +0300

    Exersise #23 from ru.code-basics
    JavaScript: выражения в определениях

---
+++ 23.js
@@ -0,0 +1,13 @@
+let eurosCount = 100;
+
+//BEGIN
+let dollarsPerEuro = 1.25;
+let rublesPerDollar = 60;
+
+let dollarCount = eurosCount * dollarsPerEuro;
+console.log(dollarCount);
+
+let rublesCount = dollarCount * rublesPerDollar;
+console.log(rublesCount);
+
+// END
