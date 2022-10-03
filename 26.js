d5008afa51ca084600bdbe651d99e9b99808e0f9
Andrei <106683554+AndreiSheleg@users.noreply.github.com>
2022-10-03 09:23:27 +0300

    task 26
    from ru.code-basics.com

---
+++ 26.js
@@ -0,0 +1,9 @@
+let king = 'King Balon the 6th';
+
+//BEGIN
+let numberOfCastles = 6;
+let numberOfRooms = 17;
+
+console.log(king + 'has ' + (numberOfCastles * numberOfRooms) + ' rooms.');
+
+//END
