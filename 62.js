6429a67954087fa9ac27a26eb4031d3a8657d941
Andrei <106683554+AndreiSheleg@users.noreply.github.com>
2022-10-06 14:17:41 +0300

    Task 62 from ru.code-basics.com

---
+++ 62.js
@@ -0,0 +1,14 @@
+// BEGIN (write your solution here)
+const getNumberExplanation = (number) => {
+  switch (number) {
+    case 666:
+      return 'devil number';
+      case 42:
+      return 'answer for everything';
+      case 7:
+      return 'prime number';
+      default:
+      return null;
+  }
+}
+// END
